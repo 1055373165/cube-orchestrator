d85e426e764f913be46741a7a10f1cafdbc91dcd
smy <1055373165@qq.com>
2024-09-05 06:53:44 +0800

    add scheduler skeleton code

--- manager/manager.go
+++ manager/manager.go
@@ -8,6 +8,9 @@ import (
 	"github.com/google/uuid"
 )
 
+// 1. Accept requests from users to start or stop tasks
+// 2. Schedule tasks onto worker machines
+// 3. Keep track of tasks, their states, and the machine on which they run
 type Manager struct {
 	Pending       queue.Queue
 	TaskDb        map[string][]*task.Task
